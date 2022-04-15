51af3ab58f3dac5b111569d7f6e59572e9d7b45e
istiyak <istiyak.milon@spectrum-bd.com>
2022-04-15 13:31:23 +0600

    first commit

---
+++ src/main/java/com/algorizin/mentorship/MentorshipAppApplication.java
@@ -0,0 +1,13 @@
+package com.algorizin.mentorship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorshipAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorshipAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algorizin/mentorship/MentorshipAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algorizin.mentorship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorshipAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
